80566a0bbad212bd86fd667d268febf05a9b14af
김상훈 <yofp1937@naver.com>
2025-11-18 15:22:03 +0900

    Todo List 베이스 구현

---
+++ TODO LIst/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TODO_LIst
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Todos todos;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            todos = new Todos();
+            // todos 타겟팅
+            DataContext = todos;
+        }
+
+        private void OnclickAddTodoBtn(object sender, RoutedEventArgs e)
+        {
+            TODO todo = new TODO()
+            {
+                Title = NewTodoTextBox.Text
+            };
+            todos.AllTodos.Add(todo);
+        }
+    }
+}
